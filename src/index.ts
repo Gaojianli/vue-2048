1b88ad83f40e2f55fb6c4ea07e17d4d5df472357
zhangyibo.114514 <zhangyibo.114514@bytedance.com>
2024-08-16 18:33:51 +0800

    export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 import Vue2048 from './App.vue'
-export default Vue2048
+import type { App, Plugin } from 'vue'
+const install = (app: App) => {
+  app.component(Vue2048.name!!, Vue2048)
+}
+
+Vue2048.install = install
+
+export { Vue2048 }
+export default Vue2048 as unknown as Plugin
--- src/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'vue-2048.vue' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
